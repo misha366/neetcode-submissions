5f904fa915a6c0165abdadf7c33ade68d347ba5e
Misha Moskalenko <misha.moskalenko.jv@gmail.com>
2026-07-18 11:38:41 +0300

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    reverseList(head: ListNode | null): ListNode {
+        if (head === null) return null;
+        let current = head;
+        let next = current.next;
+        let prev = null;
+        // let nodes: ListNode[] = [];
+        while (current !== null) {
+            let next = current.next;
+            current.next = prev;
+            prev = current;
+            if (next === null) {
+                return current;
+            }
+            current = next;
+
+        }
+    }
+}
